e55b0f9d5b1fdc0a621dba7b425d9cb4ee4e35ba
ec16555 <s.y.lam@se16.qmul.ac.uk>
2018-03-09 16:02:59 +0000

    update the application base

--- app/src/main/java/com/example/sherisesinyeelam/seprojectbase/Page_01.java
+++ app/src/main/java/com/example/sherisesinyeelam/seprojectbase/Page_01.java
@@ -2,23 +2,101 @@ package com.example.sherisesinyeelam.seprojectbase;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import java.lang.annotation.Annotation;
 
 /**
  * Created by SheriseSinYeeLam on 21/1/2018.
  */
 
 public class Page_01 extends Fragment{
+    // extents AppCompatActivity
+
+    Button btn1;
+    Button btn2;
+
+    TextView txt;
+
+    AppCompatActivity aca;
+
+    FloatingActionButton fab_plus, fab_meal, fab_sport;
+    Animation FabOpen, FabClose, FabRClockwise, FabRanticlockwise;
+    boolean isOpen = false;
 
     public Page_01(){}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View PageOne = inflater.inflate(R.layout.page1, container, false);
+        View pageOne = inflater.inflate(R.layout.page1, container, false);
+        super.onCreate(savedInstanceState);
 
-        return PageOne;
+        txt= pageOne.findViewById(R.id.test_txt);
+
+        aca = new AppCompatActivity();
+        fab_plus = (FloatingActionButton) pageOne.findViewById(R.id.fab_plus);
+        fab_meal = (FloatingActionButton) pageOne.findViewById(R.id.fab_meal);
+        fab_sport = (FloatingActionButton) pageOne.findViewById(R.id.fab_sport);
+        FabOpen = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_open);
+        FabClose =  AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.fab_close);
+        FabRClockwise = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rotate_clockwise);
+        FabRanticlockwise = AnimationUtils.loadAnimation(getActivity().getApplicationContext(), R.anim.rotate_anticlockwise);
+
+        btn1 = pageOne.findViewById(R.id.add_entry);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txt.setText("Here shows all entry.");
+
+                fab_plus.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if(isOpen){
+                            fab_sport.startAnimation(FabClose);
+                            fab_meal.startAnimation(FabClose);
+                            fab_plus.startAnimation(FabRanticlockwise);
+                            fab_meal.setClickable(false);
+                            fab_sport.setClickable(false);
+                            isOpen = false;
+                        }
+                        else{
+                            fab_sport.startAnimation(FabOpen);
+                            fab_meal.startAnimation(FabOpen);
+                            fab_plus.startAnimation(FabRClockwise);
+                            fab_meal.setClickable(true);
+                            fab_sport.setClickable(true);
+                            isOpen = true;
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+        btn2 = pageOne.findViewById(R.id.show_progress);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txt.setText("Here shows the progress graph.");
+            }
+        });
+
+
+        return pageOne;
     }
+
+
 }
